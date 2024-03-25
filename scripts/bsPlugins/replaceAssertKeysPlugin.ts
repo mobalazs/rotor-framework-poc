1a639edaf017663b9486cc5800250638f7700ea9
Balázs Molnár <mail.molnarbalazs@gmail.com>
2024-03-25 15:51:34 +0100

    utilize bsc v1 - in progress

--- scripts/bsPlugins/replaceAssertKeysPlugin.ts
+++ scripts/bsPlugins/replaceAssertKeysPlugin.ts
@@ -1,68 +1,84 @@
 import {
-  CompilerPlugin,
-  BeforeFileTranspileEvent,
-  isBrsFile,
-  WalkMode,
-  createVisitor,
-  TokenKind,
-} from "brighterscript";
+	CompilerPlugin,
+	BeforeFileAddEvent,
+	isBrsFile,
+	WalkMode,
+	createVisitor,
+	TokenKind,
+} from 'brighterscript';
 
-import path from "path";
+import path from 'path';
 
-var keypath = require('obj-keypath')
+var keypath = require('obj-keypath');
 
-const rootDir = path.join(__dirname, "../..");
-const sourcePath = path.join(rootDir, "assetsJs");
+const rootDir = path.join(__dirname, '../..');
+const sourcePath = path.join(rootDir, 'assetsJs');
 
-const getThemes = require(path.join(sourcePath, "themes"));
-const getTranslation = require(path.join(sourcePath, "translation"));
+const getThemes = require(path.join(sourcePath, 'themes'));
+const getTranslation = require(path.join(sourcePath, 'translation'));
 
 var assets = {
-  theme: getThemes(),
-  text: getTranslation(),
+	theme: getThemes(),
+	text: getTranslation(),
 };
 
 // plugin factory
 export default function () {
-  return {
-    name: "replacePlaceholders",
-    // transform AST before transpilation
-    beforeFileTranspile: (event: BeforeFileTranspileEvent) => {
-      if (isBrsFile(event.file)) {
-        event.file.ast.walk(
-          createVisitor({
-            LiteralExpression: (literal) => {
-              // replace keypath
+	return {
+		name: 'replacePlaceholders',
+		// transform AST before transpilation
+		BeforeFileAdd: (event: BeforeFileAddEvent) => {
+			if (isBrsFile(event.file)) {
+				event.file.ast.walk(
+					createVisitor({
+						LiteralExpression: (literal) => {
+							// replace keypath
 
-              if (literal.token.kind === TokenKind.StringLiteral) {
-                let re = new RegExp("({#(theme|text)=)(.*)(})", "i");
-                let textRe: any = re.exec(literal.token.text);
+							if (
+								literal.tokens.value.kind ===
+								TokenKind.StringLiteral
+							) {
+								let re = new RegExp(
+									'({#(theme|text)=)(.*)(})',
+									'i'
+								);
+								let textRe: any = re.exec(
+									literal.tokens.value.text
+								);
 
-                if (Array.isArray(textRe) && textRe.length === 5) {
-                  let assetType: string = textRe[2]; // e.g.: 'translation'
-                  let keyPath: string = textRe[3]; // e.g.: 'en.buttons.buttonText'
+								if (
+									Array.isArray(textRe) &&
+									textRe.length === 5
+								) {
+									let assetType: string = textRe[2]; // e.g.: 'translation'
+									let keyPath: string = textRe[3]; // e.g.: 'en.buttons.buttonText'
 
-                  if (
-                    textRe !== null &&
-                    (assetType === `theme` || assetType === `text`)
-                  ) {
-                    let replacement: any = keypath.get(assets[assetType], keyPath);
-                    let replacementStr: string = JSON.stringify(replacement);
+									if (
+										textRe !== null &&
+										(assetType === `theme` ||
+											assetType === `text`)
+									) {
+										let replacement: any = keypath.get(
+											assets[assetType],
+											keyPath
+										);
+										let replacementStr: string =
+											JSON.stringify(replacement);
 
-                    event.editor.overrideTranspileResult(
-                      literal,
-                      replacementStr
-                    );
-                  }
-                }
-              }
-            },
-          }),
-          {
-            walkMode: WalkMode.visitExpressionsRecursive,
-          }
-        );
-      }
-    },
-  } as CompilerPlugin;
+										event.file.editor?.overrideTranspileResult(
+											literal,
+											replacementStr
+										);
+									}
+								}
+							}
+						},
+					}),
+					{
+						walkMode: WalkMode.visitExpressionsRecursive,
+					}
+				);
+			}
+		},
+	} as CompilerPlugin;
 }
